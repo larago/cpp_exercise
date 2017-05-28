9259eeedcd47373363dd2986e414e7eb9496ded4
larago <bingeye@foxmail.com>
2017-05-28 22:47:48 +0800

    math in cpp

---
+++ math_example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    short s = 10;
+    int i = -1000;
+    long l = 100000;
+    float f = 245.55;
+    double d = 234.456;
+
+    cout << "sin(d): " << sin(d) << endl;
+    cout << "abs(i): " << abs(i) << endl;
+    cout << "floor(d): " << floor(d) << endl;
+    cout << "sqrt(f): " << sqrt(f) << endl;
+    cout << "pow(d, 2): " << pow(d, 2) << endl;
+
+    return  0;
+}
