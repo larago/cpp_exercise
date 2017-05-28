9717b10c1d4de5c6d65bda30410c7cae8a3c6ffc
larago <bingeye@foxmail.com>
2017-05-28 20:02:55 +0800

    static type in cpp

---
+++ static_example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void func(void);
+
+static int count = 10;
+
+int main() {
+    while(count--) {
+        func();
+    }
+    return 0;
+}
+
+void func(void) {
+    static int i = 5;
+    i++;
+    cout << "Value of i is: " << i;
+    cout << "Value of count is: " << count << endl;
+}
