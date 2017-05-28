dd853c0f74de074dd56d26b8b599b508275ad98b
larago <bingeye@foxmail.com>
2017-05-28 22:50:13 +0800

    random number in cpp

---
+++ random_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    int i, j;
+
+    srand( (unsigned)time(NULL) );
+
+    for (i = 0; i < 10; i++) {
+        j = rand();
+        cout << "Random number is: " << j << endl;
+    }
+
+    return 0;
+}
