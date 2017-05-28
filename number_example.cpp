d68e360f8f80ea079c734d524a8c2bb098229124
larago <bingeye@foxmail.com>
2017-05-28 22:44:37 +0800

    numbers

---
+++ number_example.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // define numbers
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+
+    s = 10;
+    i = 1000;
+    l = 1000000;
+    f = 230.56;
+    d = 45634.564;
+
+    cout << "short s is: " << s << endl;
+    cout << "int i is: " << i << endl;
+    cout << "long l is: " << l << endl;
+    cout << "float f is: " << f << endl;
+    cout << "double d is: " << d << endl;
+
+    return 0;
+}
