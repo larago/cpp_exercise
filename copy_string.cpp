d4cf800886b09ba588d83b5605975fc9aa88117f
larago <bingeye@foxmail.com>
2017-05-29 20:41:34 +0800

    string exmaples in cpp

---
+++ copy_string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str1[11] = "Hello";
+    char str2[11] = "World";
+    char str3[11];
+    int len;
+
+    strcpy(str3, str1);
+    cout << "strcpy(str3, str1): " << str3 << endl;
+
+    strcat(str1, str2);
+    cout << "strcat(str1, str2): " << str1 << endl;
+
+    len = strlen(str1);
+    cout << "strlen(str1): " << len << endl;
+
+    return 0;
+}
