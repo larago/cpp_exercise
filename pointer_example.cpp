6f4e0e8a586f912e467df782650fdeba704d468f
larago <bingeye@foxmail.com>
2017-05-29 20:55:24 +0800

    pointer in cpp

---
+++ pointer_example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int var1;
+    char var2[10];
+
+    cout << "var1's address: ";
+    cout << &var1 << endl;
+
+    cout << "var2's address: ";
+    cout << &var2 << endl;
+
+    return 0;
+}
